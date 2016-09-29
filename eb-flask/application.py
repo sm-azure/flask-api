a9523ca6cd3fd90840748aa858ee6941e0ff0101
Arnab <Arnab@FishingWS>
2016-09-29 07:08:19 +0530

    add user to flask app

--- eb-flask/application.py
+++ eb-flask/application.py
@@ -1,5 +1,32 @@
-from flask import Flask
+from flask import Flask, abort, request, jsonify, g, url_for
+from flask_sqlalchemy import SQLAlchemy
+from model.billingmodel import db
+from model.billingmodel import User, ManagedAccount, VPNTunnel
+
 application = Flask(__name__)
+
+@application.route('/api/users', methods = ['POST'])
+def new_user():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    if email is None or password is None:
+        abort(400)  # missing args
+    if User.query.filter_by(email=email).first() is not None:
+        abort(400)  # existing user
+    user = User(email)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({'email': email}, 201, {'Location':url_for('get_user', id = user.id, _external = True)})
+
+
+@application.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.email})
+
 
 @application.route('/')
 def index():
@@ -9,10 +36,6 @@ def index():
 def hello_world():
     return 'Hello, World!'
 
-@application.route('/user/<username>')
-def user(username):
-    return 'User %s' % username
-
 @application.route('/post/<int:post_id>')
 def post(post_id):
     return 'Post %d' % post_id
@@ -20,5 +43,5 @@ def post(post_id):
 
 if __name__ == '__main__':
     application.debug = True
-    #application.run(host='0.0.0.0', port=3000)
-    applciation.run()
+    application.run(host='0.0.0.0', port=3000)
+    #application.run()
